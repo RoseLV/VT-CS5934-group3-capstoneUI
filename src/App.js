2d85c0c13b5fc16df276407a1f441309c2857efa
shekharmnnit <shekhark221@gmail.com>
2024-12-08 13:59:32 -0500

    shek changes

--- src/App.js
+++ src/App.js
@@ -29,8 +29,57 @@ const LandingPage = () => {
       const fallbackData = await fallbackResponse.json();
 
       // Extract prediction from the first result in the response array
-      const fallbackPrediction = fallbackData[0]?.fake_news_prediction || null;
+      const fallbackPrediction = fallbackData[0]?.fake_news_prediction=="True"? "False":"True" || null;
       return fallbackPrediction;
+    } catch (fallbackError) {
+      console.error("Fallback API error:", fallbackError);
+      return null; // Return null for invalid cases; // Final fallback value
+    }
+  };
+
+  // Mock API call to determine if the text is "Real" or "Fake"
+  const fetchvalidate = async (text) => {
+    try {
+      var fallbackResponse = await fetch(
+        "http://cs5934-offline-group3-alb-985856241.us-east-1.elb.amazonaws.com/validate",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ vtext: text}), // The /predict endpoint expects a "texts" array
+        }
+      );
+
+      if (!fallbackResponse.ok) throw new Error("Fallback API call failed");
+
+      var fallbackData = await fallbackResponse.json();
+
+      // Extract prediction from the first result in the response array
+      var validateResponse = [];// array of obj
+      if(fallbackData.is_valid == true){
+        validateResponse = fallbackData?.similar_articles;
+      }
+      else{
+        validateResponse=[{
+          "title": "No similar articles found",
+          "link": "",
+          "similarity": 0
+        }]
+      }
+      let maxSimilarityValue = Math.max(...validateResponse.map(article => article.similarity));
+      let maxSimilarityValuePrecentage = Math.ceil(maxSimilarityValue * 10000) / 100 + " %";
+
+      // Generate the HTML for the list of related news articles
+      const relatedNewsHTML = validateResponse.map(article => {
+          return `<li><a href="${article.link}" target="_blank">${article.title}</a></li>`;
+      }).join('');
+
+      // Insert the HTML into the document for validated news
+      document.getElementById('accuracy').innerText = `Alignment Percentage: ${maxSimilarityValuePrecentage}`;
+      document.getElementById('related-news').innerHTML = relatedNewsHTML;
+
+      return validateResponse;
     } catch (fallbackError) {
       console.error("Fallback API error:", fallbackError);
       return null; // Return null for invalid cases; // Final fallback value
@@ -78,8 +127,29 @@ const LandingPage = () => {
       message.warning("Please enter some text before submitting.");
       return;
     }
-    const result = await fetchFakeOrReal(text);
+    document.getElementById('loader').style.display = 'block';
+
+    // const result = await fetchFakeOrReal(text);
+    // setFakeNews(result);
+
+    // // call Sentiment
+    // const resultSentiment = await fetchSentiment(text);
+    // setIsPositive(resultSentiment);
+
+    // //call validate api
+    // const validateResponse = await fetchvalidate(text);
+
+    const [result, resultSentiment, validateResponse] = await Promise.all([
+      fetchFakeOrReal(text),
+      fetchSentiment(text),
+      fetchvalidate(text)
+    ]);
+  
     setFakeNews(result);
+    setIsPositive(resultSentiment);
+
+    document.getElementById('loader').style.display = 'none';
+    
     message.success(`The text is classified as: ${result}`);
   };
 
@@ -87,6 +157,10 @@ const LandingPage = () => {
     setText("");
     setFakeNews(null);
     setIsPositive(null);
+
+    // Insert the HTML into the document for validated news
+    document.getElementById('accuracy').innerText = `Accuracy: ${0}%`;
+    document.getElementById('related-news').innerHTML = [];
   };
 
   const handleDisagree = async (label) => {
@@ -121,69 +195,57 @@ const LandingPage = () => {
 
   const throttledHandleDisagree = throttle(handleDisagree, 5000);
   return (
-    <div
-      style={{
-        textAlign: "center",
-        display: "flex",
-        flexDirection: "column",
-        height: "100vh",
-        justifyContent: "space-between",
-      }}
-    >
-      <div>
-        <h3>Misinformation Detector</h3>
-        {/* Display Result */}
-        <div style={{ margin: "20px", textAlign: "left" }}>
-          <p>
-            Fake News: {isFake}{" "}
-            {isFake && <Button type="primary" danger onClick={() => throttledHandleDisagree(isFake)}>disagree</Button>}
-          </p>
-          <p>
-            Sensiment: {isPositive}{" "}
-            {isPositive && <Button type="primary" danger onClick={() => throttledHandleDisagree(isPositive)}>disagree</Button>}
-          </p>
+    <div className="container">
+      {/* Left Section */}
+      <div className="left-section">
+        <h1 className="header">Misinformation Detector</h1>
+        <TextArea
+          placeholder="Input Text..."
+          className="text-area"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          autoSize={{
+            minRows: 6,
+            maxRows: 10,
+          }}
+        />
+        <div className="button-group">
+          <Button type="primary" onClick={handleFakeNewsClick}>
+            Check Fake News
+          </Button>
+          {/* <Button type="primary" onClick={handleSentimentClick}>
+            Check Sentiment
+          </Button> */}
+          <Button type="default" onClick={handleReset}>
+            Reset
+          </Button>
         </div>
       </div>
 
-      <div
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "space-around",
-          alignItems: "center",
-          width: "100%",
-        }}
-      >
-        {/* Text Input Section */}
-        <TextArea
-          placeholder="Enter something..."
-          style={{ width: "300px", marginBottom: "2em" }}
-          value={text}
-          autoSize={{
-            minRows: 6,
-            maxRows: 20,
-          }}
-          onChange={(e) => setText(e.target.value)}
-        />
-
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "space-around",
-            alignItems: "center",
-            width: "100%",
-          }}
-        >
-          {/* Submit Button */}
-          <Button type="primary" onClick={throttle(handleFakeNewsClick,3000)}>
-            Check Fake News
-          </Button>
-          <Button type="primary" onClick={throttle(handleSentimentClick,3000)}>
-            Check Sentiment
-          </Button>
-          <Button type="primary" onClick={handleReset}>
-            Reset
-          </Button>
+      {/* Right Section */}
+      <div className="right-section">
+      <div id="loader" className="loader" style={{ color: 'red',display:'none' }}><h1>Loading.....</h1></div>
+        <h1>Result</h1>
+        <p>
+          <b>Fake News:</b> <span className={isFake === "Negative" ? "negative" : "positive"}>{isFake}</span>
+        </p>
+        <p>
+          <b>Sentiment:</b> <span className={isPositive === "Positive" ? "positive" : "negative"}>{isPositive}</span>
+        </p>
+        
+        <h2>Ground Truth Output</h2>
+        <p>
+          {/* <b>Accuracy:</b> 30% */}
+          <div id="accuracy"></div>
+        </p>
+        <div className="related-news">
+          <b>Related News:</b>
+          {/* <ul>
+            <li>Apple</li>
+            <li>Cat</li>
+            <li>Tot</li>
+          </ul> */}
+          <ul id="related-news"></ul>
         </div>
       </div>
     </div>
